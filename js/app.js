4c58fdd8984fa956d267d5af8b2167ae0eb5ecb3
bool_one <41709736+jpstories@users.noreply.github.com>
2020-03-16 16:34:58 +1000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/* https://greensock.com/gsap */
+TweenLite.set("#petals", { perspective:600 })
+TweenLite.set("img", { xPercent:"-50%", yPercent:"-50%" })
+
+var total = 20;
+var warp = document.getElementById("petals"),
+w = window.innerWidth,
+h = window.innerHeight;
+
+for (i = 0; i < total; i++) {
+    var Div = document.createElement('div');
+    TweenLite.set(Div, { attr: { class:'dot' }, x:R(0,w), y:R(-200,-150), z:R(-200,200)});
+    warp.appendChild(Div);
+    animm(Div);
+}
+
+function animm(elm) {
+    TweenMax.to(elm,R(6,15), { y:h+100, ease:Linear.easeNone, repeat:-1, delay:-15 });
+    TweenMax.to(elm,R(4,8), { x:'+=100', rotationZ:R(0,180), repeat:-1, yoyo:true, ease:Sine.easeInOut });
+    TweenMax.to(elm,R(2,8), { rotationX:R(0,360), rotationY:R(0,360), repeat:-1, yoyo:true, ease:Sine.easeInOut, delay:-5 });
+};
+
+function R(min,max) { return min+Math.random() * (max-min) };
+
+
+/* https://mattboldt.com/typed.js/ */
+var typed = new Typed('#text', {
+    strings: ['こんにちは、私の名前はSergeyです。私はWebアプリケーション開発者です。 今、あなたの前にいるのは私の創作の一つです。 私はこれを2年間ずっとこのような不名誉なことをしてきました。 ご清聴ありがとうございました！'],
+    startDelay: 3000,
+    typeSpeed: 80,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: false,
+    showCursor: false,
+    onComplete: function() {
+        var author = document.getElementById("author");
+        author.style.opacity = 1;
+    }
+});
